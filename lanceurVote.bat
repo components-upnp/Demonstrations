b95359021b5c3092675399ad42bef2bd3f8e0591
MKostiuk <Mat01061995>
2017-07-06 15:04:14 +0200

    Premier commit contenant les trois scripts pour les démos Vote, Caméra et diapositives

---
+++ lanceurVote.bat
@@ -0,0 +1,41 @@
+﻿@echo off
+
+Rem Suppression des beans déjà créés
+cd %USERPROFILE%
+del Documents\wcomp.net\beans\*
+
+Rem Lancement Polling Station
+cd %USERPROFILE%
+cd IdeaProjects\Java_Bureau_De_Vote\target\Java_Bureau_De_Vote-1.0-SNAPSHOT-zip
+start lanceur.bat
+
+Rem Lancement IHM
+cd %USERPROFILE%
+cd IdeaProjects\Java_Polling_Station_IHM\target
+start Java_Polling_Station_IHM-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+Rem Lancement Générateur de rapport
+cd %USERPROFILE%
+cd IdeaProjects\GenerateurDeRapport\target
+start GenerateurDeRapport-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+Rem Lancement Int to Direction adaptateur
+cd %USERPROFILE%
+cd IdeaProjects\Java_Int_To_Direction_Adapter\target\Java_Int_To_Direction_Adapter-1.0-SNAPSHOT-zip
+start lanceur.bat
+
+Rem Lancement script assemblage vote interactif 
+cd %USERPROFILE%
+cd IdeaProjects\Scripts_UserStory\target
+start lanceurAssemblageVote.bat
+
+Rem Lancement du wizard designer
+cd C:\Program Files (x86)\SharpDevelop\3.0\UPnPWizardDesigner
+start UPnPWizardDesigner.exe
+
+Rem Lancement de WComp
+cd C:\ProgramData\Microsoft\Windows\Start Menu\Programs\SharpDevelop 3.2
+start SharpDevelop
+
+Rem Exit
+exit
